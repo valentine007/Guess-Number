b1aecda0c7d39af9412b2499b4810a295ec57bec
valentine007 <vdemchuk.vd@gmail.com>
2017-03-21 23:52:47 +0200

    Игра реализована в цвете Доработано меню Дорабатывается воздействие яда на игровой процесс

--- Guess_Number/Form1.Designer.cs
+++ Guess_Number/Form1.Designer.cs
@@ -34,7 +34,7 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numberField = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -101,7 +101,7 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.numberField);
             this.groupBox2.Location = new System.Drawing.Point(164, 13);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(112, 99);
@@ -117,13 +117,15 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Підтверджую";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // numberField
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(99, 20);
-            this.textBox1.TabIndex = 0;
+            this.numberField.Location = new System.Drawing.Point(7, 20);
+            this.numberField.Name = "numberField";
+            this.numberField.Size = new System.Drawing.Size(99, 20);
+            this.numberField.TabIndex = 0;
+            this.numberField.TextChanged += new System.EventHandler(this.numberField_TextChanged);
             // 
             // label1
             // 
@@ -142,6 +144,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(263, 174);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
@@ -189,6 +192,7 @@
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // button2
             // 
@@ -267,7 +271,7 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox numberField;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
