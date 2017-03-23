e802fe2ea0d70dfe83c83457cc19e68dab3a468b
valentine007 <vdemchuk.vd@gmail.com>
2017-03-24 00:31:18 +0200

    labels fill added

--- Guess_Number/Form1.Designer.cs
+++ Guess_Number/Form1.Designer.cs
@@ -178,6 +178,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
--- Guess_Number/Form1.cs
+++ Guess_Number/Form1.cs
@@ -15,8 +15,10 @@ namespace Guess_Number
         int number = 0; /// <summary>
                         /// Переменная для хранения числа заданного пользователем
                         /// </summary>
+        int level = 0;  // переменная для цикла
         int randNumber = 0; // переменная для хранения числа заданного компьютером
         int counter = 0;    // переменная - счетчик позиционных совпадений
+        static int tryes = 0;   // переменная - счетчик для хранения общего количества попыток
 
         public Form1()
         {
@@ -33,6 +35,7 @@ namespace Guess_Number
             if (radioButton1.Checked)
             {
                 Random r1 = new Random();
+                level = 3;
                 randNumber = r1.Next(100, 999);
                 MessageBox.Show(randNumber.ToString());
             }
@@ -43,6 +46,7 @@ namespace Guess_Number
             if (radioButton2.Checked)
             {
                 Random r2 = new Random();
+                level = 5;
                 randNumber = r2.Next(10000, 99999);
                 MessageBox.Show(randNumber.ToString());
             }
@@ -50,9 +54,13 @@ namespace Guess_Number
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            Random r3 = new Random();
-            randNumber = r3.Next(1000000, 9999999);
-            MessageBox.Show(randNumber.ToString());
+            if (radioButton3.Checked)
+            {
+                Random r3 = new Random();
+                level = 7;
+                randNumber = r3.Next(1000000, 9999999);
+                MessageBox.Show(randNumber.ToString());
+            }
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
@@ -72,7 +80,10 @@ namespace Guess_Number
 
         private void button1_Click(object sender, EventArgs e)
         {
+            tryes++;
+            label6.Text = tryes.ToString();
             MessageBox.Show(number.ToString());
+            textBox3.Text = number.ToString();
             if (number == randNumber)
             {
                 MessageBox.Show("Вітаю! Ви перемогли! :)");
@@ -81,7 +92,7 @@ namespace Guess_Number
             {
                 int temp1 = 0;
                 int temp2 = 0;
-                for (int i = 0; i < 5; ++i)
+                for (int i = 0; i < level; ++i)
                 {
                     temp1 = number % 10;
                     temp2 = randNumber % 10;
@@ -91,7 +102,14 @@ namespace Guess_Number
                     randNumber /= 10;
                 }
                 MessageBox.Show(counter.ToString());
+                textBox5.Text = counter.ToString();
+                counter = 0;
             }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
